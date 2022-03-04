04a5075c14ad68361de269c92d3e8b74233dceeb
Aleksei <katakuxiko@gmail.com>
2022-03-04 18:38:57 +0600

    Food site project

---
+++ food/js/js.js
@@ -0,0 +1,337 @@
+window.addEventListener('DOMContentLoaded',()=>{
+	'use strict';
+	//tabs
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		  tabsContent = document.querySelectorAll('.tabcontent'),
+		  tabsParent = document.querySelector('.tabheader__items');
+	console.log(tabsContent);
+	function hideTabContent(){
+		tabsContent.forEach(item =>{
+			// item.style.display = 'none';
+			item.classList.add('hide');
+			item.classList.remove('show','fade');
+		});
+		tabs.forEach(item=>{
+			
+			item.classList.remove('tabheader__item_active');
+		});
+	}
+	
+	function showTabContent(i = 0){
+		// tabsContent[i].style.display='block';
+		tabsContent[i].classList.add('show','fade','l');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add('tabheader__item_active');
+
+	}
+
+	hideTabContent();
+	showTabContent(0);
+
+	tabsParent.addEventListener('click',event =>{
+		const target = event.target;
+
+		if(target&&target.classList.contains('tabheader__item')){
+			tabs.forEach((item,i) =>{
+				if(target == item){
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+
+	});
+	//timer 
+
+	const deadline = '2022-5-12';
+
+	
+	function getTimeRemaining(endTime) {
+		const t = Date.parse(endTime) - Date.parse(new Date()),
+			  days = Math.floor(t /(1000*60*60*24)),
+			  hours = Math.floor((t/ (1000*60*60))%24),
+			  minutes = Math.floor((t/60/1000)%60),
+			  seconds = Math.floor((t/1000)%60);
+
+		return{
+			'total':t,
+			'days': days,
+			'hours':hours,
+			'minutes':minutes,
+			'seconds':seconds
+		};
+	}
+
+	function getZero(num){
+		if(num>=0&& num<10){
+			return `0${num}`;
+
+		}else{
+			return num;
+		}
+	}
+
+ 	
+	function setClock(selector,endTime) {
+			const timer = document.querySelector(selector),
+				  days = timer.querySelector('#days'),
+				  hours = timer.querySelector('#hours'),
+				  minutes = timer.querySelector('#minutes'),
+				  seconds = timer.querySelector('#seconds'),
+				  timeInter = setInterval(updateClock,1000);
+				updateClock();
+			
+			
+			function updateClock(){
+				const t = getTimeRemaining(endTime);
+
+				days.innerHTML =getZero(t.days);
+				hours.innerHTML = getZero(t.hours);
+				minutes.innerHTML =getZero (t.minutes);
+				seconds.innerHTML = getZero(t.seconds);
+				if(t.total <= 0){
+					clearInterval(timeInter);
+				}
+			}
+	}
+
+		console.log(getTimeRemaining(deadline));
+		setClock('.timer', deadline);
+    
+
+	//modal
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		  modal = document.querySelector('.modal'),
+		  modalTriggerClose = modal.querySelector('[data-close]');
+
+	// modalTrigger.forEach(element =>{
+	// 	element.addEventListener('click',()=>{
+	// 		console.log('dsadsa');
+	// 		modal.style.display = 'flex';
+	// 	});
+	// modalTriggerClose.addEventListener('click',()=>{
+	// 	modal.style.display = 'none';
+	// });
+	// });
+
+	modalTrigger.forEach(element =>{
+		element.addEventListener('click',()=>{
+			openModal();
+		});
+	});
+
+	function closeModal() {
+	modal.classList.add('hide');
+		modal.classList.remove('show');
+		document.body.style.overflow = '';	
+	}
+
+	function openModal(){
+		modal.classList.add('show');
+			modal.classList.remove('hide');
+			document.body.style.overflow = 'hidden';
+			clearInterval(modalTimerId);
+	}
+
+	modalTriggerClose.addEventListener('click',closeModal);
+
+	modal.addEventListener('click',(e)=>{
+		if(e.target === modal){
+			closeModal();
+		}
+	});
+
+	
+
+	document.addEventListener('keydown',(e)=>{
+		if(e.code ==="Escape" && modal.classList.contains('show')){
+			closeModal();
+		}
+	});
+
+	// const modalTimerId = setTimeout(openModal,5000);
+	
+	function showModalByScroll(){
+			if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1)
+			{
+				openModal();
+				window.removeEventListener('scroll',showModalByScroll);
+		}
+	}
+
+	window.addEventListener('scroll',showModalByScroll);
+
+	
+
+	//Клаассы карточек
+	class MenuCard {
+		constructor(src,alt,title,descr,price,parentSelector, ...classes) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 475;
+			this.classes = classes;
+			this.changeToKZ();
+		}
+		
+		changeToKZ(){
+			this.price = this.price*this.transfer;
+		}
+
+		render(){
+			const element = document.createElement('div');
+			if(this.classes.length===0){
+				this.element = "menu__item";
+				element.classList.add(this.element);
+			} else{
+				this.classes.forEach(className => element.classList.add(className));
+			}
+			
+			
+			element.innerHTML = `
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> тг/день</div>
+				</div>
+                
+			`;
+			this.parent.append(element);
+		}
+	
+
+	
+	}
+	new MenuCard("img/tabs/vegy.jpg",
+	"vegy",`Меню "Фитнес"`,`Меню "Фитнес" - это новый подход к приготовлению блюд:
+	 больше свежих овощей и фруктов. Продукт активных и здоровых людей.
+	  Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+    9,
+	'.menu .container'
+	).render();
+	new MenuCard("img/tabs/elite.jpg","elite",
+	`Меню “Премиум”`,`В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
+	24,'.menu .container','menu__item',"big").render();
+	new MenuCard("img/tabs/post.jpg","post",
+		'Меню "Постное"','Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		15,'.menu .container','menu__item',"big").render();
+
+
+	class Slider{
+		constructor(scr,alt){
+			this.scr = scr;
+			this.alt = alt;
+		}
+		render(){
+			slide.innerHTML = `<div class="offer__slide">
+                        <img src=${this.scr} alt=${this.alt}>
+                    </div>
+			`;
+		} 
+		
+	}
+
+//slider
+
+	// const slider = document.querySelector('.offer__slider'),
+	//   sliderPrev = slider.querySelector('.offer__slider-prev'),
+	//   sliderNext = slider.querySelector('.offer__slider-next'),
+	//   slide = document.querySelector('.offer__slide'),
+	//   current = slider.querySelector('#current'),
+	//   total = slider.querySelector('#total');
+
+	//   let currentID = 3;
+	//   sliderPrev.addEventListener('click', () =>{
+	// 	currentID--;
+	// 	if(currentID <= 0){
+	// 		currentID = allSlides.length;
+	// 	}
+	// 	allSlides[currentID-1].render();
+	// 	console.log(current);
+	// 	current.textContent = getZero(currentID);
+	//   });
+	//   sliderNext.addEventListener('click', () =>{
+	// 	currentID++;
+	// 		if(currentID >= allSlides.length+1){
+	// 		currentID = 1;
+	// 	}
+	// 	console.log(current);
+	// 	allSlides[currentID-1].render();
+	// 	current.textContent = getZero(currentID); 
+	
+		
+	//   });
+	
+	// console.log(slider,sliderNext);
+	// const slide1 = new Slider("img/slider/olive-oil.jpg","oil"),
+	// 	 slide2 = new Slider("img/slider/food-12.jpg","food"),
+	// 	 slide3 = new Slider("img/slider/paprika.jpg","paprika"),
+	// 	 slide4 = new Slider("img/slider/pepper.jpg","pepper");
+	// let allSlides = [slide1, slide2, slide3, slide4];
+
+	  
+	// total.innerHTML = `
+	//   <span id="total">${allSlides.length}</span>`;
+	// slide1.render();
+		
+	//Forms
+
+	const forms = document.querySelectorAll('form');
+
+	const message = {
+		loading: 'Загрузка',
+		success: 'Спасибо! Скоро мы с вами свяжемся',
+		failure: 'Что-то пошло не так...'
+	};
+
+	forms.forEach(item => {
+		postData(item);
+	});
+
+	function postData(form){
+		form.addEventListener('submit',(e)=>{
+			e.preventDefault();
+			
+			const statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+			statusMessage.textContent = message.loading;
+			form.append(statusMessage);
+
+			const request = new XMLHttpRequest();
+			request.open('POST','server.php');
+
+			request.setRequestHeader('Content-type','application/json');
+			const formData = new FormData(form);
+			const object = {};
+			formData.forEach(function(value,key) {
+				object[key] = value;
+			});
+
+			const json = JSON.stringify(object);
+
+			request.send(json);
+			request.addEventListener('load',()=>{
+				if(request.status === 200){
+					console.log(request.response);
+					statusMessage.textContent = message.success;
+					form.reset();
+					setTimeout(()=>{
+						statusMessage.remove();
+					},2000);
+				}else{
+					statusMessage.textContent = message.failure;
+				}
+			});
+
+		});
+	}
+
+	});
+
